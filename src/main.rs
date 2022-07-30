ef57225c8448f181bbc8ebac36b03bc4169fe493
yuchiki <14884013+yuchiki@users.noreply.github.com>
2022-07-30 04:36:19 +0000

    tokenizerがtokenとそのpositionのタプルの列を返すように変更した

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+type SourcePosition = usize;
+
 enum Token {
     Num(i32),
     Plus,
@@ -18,17 +20,17 @@ fn main() {
     println!(".globl main");
     println!("main:");
 
-    if let [Token::Num(num), tokens @ ..] = tokens {
+    if let [(Token::Num(num), _), tokens @ ..] = tokens {
         println!("  mov rax, {num}");
 
         let mut tokens = tokens;
         while !tokens.is_empty() {
             match tokens {
-                [Token::Plus, Token::Num(num), rest @ ..] => {
+                [(Token::Plus, _), (Token::Num(num), _), rest @ ..] => {
                     println!("  add rax, {num}");
                     tokens = rest;
                 }
-                [Token::Minus, Token::Num(num), rest @ ..] => {
+                [(Token::Minus, _), (Token::Num(num), _), rest @ ..] => {
                     println!("  sub rax, {num}");
                     tokens = rest;
                 }
@@ -44,22 +46,27 @@ fn main() {
     println!("  ret");
 }
 
-fn tokenize(mut input: &[char]) -> Vec<Token> {
+fn tokenize(mut input: &[char]) -> Vec<(Token, SourcePosition)> {
     let mut ans = vec![];
+    let mut pos: SourcePosition = 0;
+
     while !input.is_empty() {
         match input {
             ['+', rest @ ..] => {
-                ans.push(Token::Plus);
+                ans.push((Token::Plus, pos));
                 input = rest;
+                pos += 1;
             }
             ['-', rest @ ..] => {
-                ans.push(Token::Minus);
+                ans.push((Token::Minus, pos));
                 input = rest;
+                pos += 1;
             }
             ['0'..='9', ..] => {
-                if let (rest, Some(num)) = munch_int(input) {
-                    ans.push(Token::Num(num));
+                if let (rest, Some(num), char_count) = munch_int(input) {
+                    ans.push((Token::Num(num), pos));
                     input = rest;
+                    pos += char_count;
                 }
             }
             _ => {
@@ -71,18 +78,20 @@ fn tokenize(mut input: &[char]) -> Vec<Token> {
     ans
 }
 
-fn munch_int(mut input: &[char]) -> (&[char], Option<i32>) {
+fn munch_int(mut input: &[char]) -> (&[char], Option<i32>, usize) {
+    let mut char_count = 0;
+
     if let ['0'..='9', ..] = input {
         let mut ans = 0;
-
         while let [digit @ '0'..='9', rest @ ..] = input {
             ans = ans * 10 + (*digit as i32) - ('0' as i32);
             input = rest;
+            char_count += 1;
         }
 
-        (input, Some(ans))
+        (input, Some(ans), char_count)
     } else {
-        (input, None)
+        (input, None, 0)
     }
 }
 
